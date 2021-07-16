651289842719adb0720a5f3a7e2dfb1f4b55090c
rparth07 <pmr642001@gmail.com>
2021-07-16 16:25:54 +0530

    Basic Functions

---
+++ opencvproject/opencvproject/Basic Functions.cpp
@@ -0,0 +1,33 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+/// <summary>
+/// /////////////////////// Basic Functions //////////////////////////////
+/// </summary>
+
+void main() {
+
+	string path = "Resources/test.png";
+	Mat img = imread(path); 
+	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
+
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+	GaussianBlur(img, imgBlur, Size(7, 7), 7, 3);
+	Canny(imgBlur, imgCanny, 15, 17);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5));
+	dilate(imgCanny, imgDil, kernel);
+	erode(imgDil, imgErode, kernel);
+
+	imshow("Image", img);
+	imshow("Image Gray", imgGray);
+	imshow("Image Blur", imgBlur);
+	imshow("Image Canny", imgCanny);
+	imshow("Image Dilation", imgDil);
+	imshow("Image Canny", imgErode); 
+
+	waitKey(0);
+}
