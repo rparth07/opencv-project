e0031d7eb8d6c8d005beee86a3d35f2950925f31
rparth07 <pmr642001@gmail.com>
2021-07-16 11:40:10 +0530

    Importing videos and access webcams

---
+++ opencvproject/enc_temp_folder/173cbfaed9913d3b4e4a8396fdc1d05a/vid-import.cpp
@@ -0,0 +1,35 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+/// <summary>
+/// /////////////////////// How to import videos //////////////////////////////
+/// </summary>
+/*
+void main() {    
+
+	string path = "Resources/test_video.mp4";
+	VideoCapture cap(path);
+	Mat img;
+	while (true) {
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(10);
+	}
+}*/
+
+///// for WebCam ////// 
+
+void main() {
+
+	VideoCapture cap(0);
+	Mat img;
+	while (true) {
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(10);
+	}
+}
